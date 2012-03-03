b827d46288ca7f19e4f4c93ece8b49716dbfd5ad
Wai Lee Chin Feman <wchinfeman@8thlight.com>
2012-03-03 12:48:55 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TttRails::Application.config.secret_token = '5ba55e5e613c2d0ba403c618bb7297db4e9fe325758e59c10d123cbe159a44f4be92f829b4afc9785638cd3c4ebd0ba817e18a217e7b86b06e72b522b298cc6e'
