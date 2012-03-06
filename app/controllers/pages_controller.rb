11570dc7bbef4293e1d5fd351ea9041523689670
Wai Lee Chin Feman <wchinfeman@8thlight.com>
2012-03-06 13:25:47 -0600

    board is now a partial

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,7 @@ class PagesController < ApplicationController
   def game
     @title="Gametime"
     @stylesheets=[params[:action]]
+    @board=[["z","o","x"],["o","o","o"],["x","o","x"]]
   end
   def about
     @title="About"
